f7c6a95fc825ab2dd000097a9f03d04558fbcb6e
kumankoishi.py <7iggif@naver.com>
2021-04-21 15:26:24 +0900

    Added and tried tslint

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 "use strict";
 function test() {
-    let comment = "TEST";
+    let comment = 'TEST';
+    comment = '5';
     console.log(comment);
 }
+class Test2 {
+    constructor() {
+        console.log('TEST 2');
+    }
+}
 test();
+new Test2();
--- test.ts
+++ test.ts
@@ -1,6 +1,14 @@
 function test() {
-  let comment: String = "TEST";
+  let comment: string = 'TEST';
+  comment = '5';
   console.log(comment);
 }
 
+class Test2 {
+  constructor() {
+    console.log('TEST 2');
+  }
+}
+
 test();
+new Test2();
