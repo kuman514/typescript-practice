c65d3925221e3f8d6a1cf9ae3ff36e810cabf2cc
kumankoishi.py <7iggif@naver.com>
2021-05-07 00:03:35 +0900

    Practicing abstract class

---
+++ absctact class.ts
@@ -0,0 +1,14 @@
+// Java에서 쓰던 abstract class처럼 하면 될 것 같다.
+abstract class AbsPerson {
+  protected _name: string = 'James';
+  abstract setName(name: string): void;
+}
+
+class ImplPerson extends AbsPerson {
+  setName(name: string): void {
+    this._name = name;
+  }
+}
+
+// abstract class는 new를 통한 인스턴스로 생성할 수 없다.
+// abstract class를 상속한 class는 반드시 abstract 메소드를 구현해야 한다.
