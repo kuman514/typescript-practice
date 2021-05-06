4a93f4e3e92be805bcdf3878a764753688ea12f2
kumankoishi.py <7iggif@naver.com>
2021-05-06 15:56:33 +0900

    Practicing getter and setter on class

---
+++ class get set.ts
@@ -0,0 +1,25 @@
+class GetSetPerson {
+  private _name: string;
+  private _age: number;
+
+  constructor(name: string, age: number) {
+    this._name = name;
+    this._age = age;
+  }
+
+  // class의 getter과 setter은 ES6의 방식대로 작성하면 된다.
+  get name() {
+    return this._name;
+  }
+
+  // 밖에서 읽기만 하고자 할땐, set을 생략한다.
+  set name(name: string) {
+    this._name = name;
+  }
+}
+
+const getset: GetSetPerson = new GetSetPerson('koishi', 1);
+
+console.log(getset.name);
+getset.name = 'koishie';
+console.log(getset.name);
